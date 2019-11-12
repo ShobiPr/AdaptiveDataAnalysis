8046a802d7896e4abe5ab6033f8f4541c91025a3
juliehaga <jul.haga@gmail.com>
2019-11-12 10:18:17 +0100

    getting data for 3 subjects

--- dataset.py
+++ dataset.py
@@ -7,7 +7,7 @@ import warnings
 warnings.filterwarnings("ignore")
 
 
-def get_subdataset(_S=1, Sess=1):
+def get_subdataset(_S, Sess):
     _file = 'Data_S%02d_Sess%02d.csv' % (_S, Sess)
     _f = open(_file).readlines()
     channels = []
@@ -16,7 +16,7 @@ def get_subdataset(_S=1, Sess=1):
             channels.append(eval(_rows))
         else:
             _headers = _rows
-        if i > 4500:
+        if i > 11500:
             return np.array(channels)
     return np.array(channels)
 
@@ -25,37 +25,16 @@ def get_samples(_index, s_s_chs, sr, _size=1.3):
     return s_s_chs[_index:int(math.ceil(_index + (_size * sr)))][:]
 
 
-def get_feedback_signal():
+def get_dataset():
     sr = 200
-    for subject in range(1, 2):  # 1
+    data = []
+    for subject in range(1, 4):  # 1
         for session in range(1, 2):  # 1
-            s_s_chs = get_subdataset(subject, session)
+            subject_dataset = get_subdataset(subject, session)
             # first instance
-            _index = 3937
-            data = get_samples(_index, s_s_chs, sr)
+            _index = [i + 1 for i, d in enumerate(subject_dataset[:, -1]) if d == 1]
+            print(_index[0])
+            all_channels = get_samples(_index[0], subject_dataset, sr)
+            data.append(all_channels[:,55])
             # channel O1
-            return data[:, 55]
-
-
-def get_feedback_signal():
-    sr = 200
-    for subject in range(1, 2):  # 1
-        for session in range(1, 2):  # 1
-            s_s_chs = get_subdataset(subject, session)
-            # first instance
-            _index = 3937
-            data = get_samples(_index, s_s_chs, sr)
-            # channel O1
-            return data[:, 55]
-
-
-def get_resting_signal():
-    sr = 200
-    for subject in range(1, 2):  # 1
-        for session in range(1, 2):  # 1
-            s_s_chs = get_subdataset(subject, session)
-            # first instance
-            _index = 3937 - 200*2
-            data = get_samples(_index, s_s_chs, sr)
-            # channel O1
-            return data[:, 55]
+    return data
